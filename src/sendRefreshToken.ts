7a025ed6229e9fa5d0ac59a89951aff5ad154c10
Olli Hirvonen <ollihirvonen@Ollis-MBP.fios-router.home>
2021-03-13 20:04:44 -0500

    cookie bs

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 export const sendRefreshToken = (res: Response, token: string): void => {
   if (res) {
     res.cookie('saID', token, {
+      sameSite: 'none',
       httpOnly: false,
       maxAge: 100000000,
       path: '/refresh_token',
