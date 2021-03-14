d66f0ddd884cab3084dd94ad7eebfb133fc11251
Olli Hirvonen <ollihirvonen@Ollis-MBP.fios-router.home>
2021-03-13 20:10:28 -0500

    more cookie bss

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -4,6 +4,7 @@ export const sendRefreshToken = (res: Response, token: string): void => {
   if (res) {
     res.cookie('saID', token, {
       sameSite: 'none',
+      secure: true,
       httpOnly: false,
       maxAge: 100000000,
       path: '/refresh_token',
